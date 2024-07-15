1b4885a6594da9b6ae4ed5e0b329e8ad3c744b5e
Nicholas do Vale Siécola <100312825+NSiecola@users.noreply.github.com>
2024-07-15 18:25:06 +0000

    Projeto 1 feito

--- CreateABackEndAppWithJavaScript/Projeto1/js/request-logic.js
+++ CreateABackEndAppWithJavaScript/Projeto1/js/request-logic.js
@@ -2,7 +2,16 @@
 // extension off of the supplied filename, but we can't figure out the rest of
 // the function to use it! We hope this is useful to you!
 
-
 function getContentType(filename) {
   const extension = filename.match(/.*\.([^\.]*)$/)[1];
+
+  if (extension === "html") {
+    return "text/html";
+  } else if (extension === "css") {
+    return "text/css";
+  } else if (extension === "jpg" || extension === "jpeg") {
+    return "image/jpeg";
+  }
+
+  return "text/plain";
 }
